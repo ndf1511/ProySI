9cf5584c3da78e9fb35c946117e4f90c9b994c60
Usuario <Usuario@OscarArcosPC>
2017-10-07 20:44:31 -0500

    Perros puse algo

--- src/Controllers/ProcesosController.java
+++ src/Controllers/ProcesosController.java
@@ -1,7 +1,17 @@
 package Controllers;
 
+import Conexiones.ReturnEntitiesConexion;
+import Entities.Inventario;
+import Entities.Maquinaria;
+import Entities.Proceso;
+import Entities.Proveedor;
+import Entities.Receta;
+import Entities.Suministro;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,6 +20,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -20,13 +31,13 @@ import javafx.stage.StageStyle;
 public class ProcesosController implements Initializable{
 
     @FXML
-    private TableColumn<?, ?> tc_timeMaquina;
+    private TableColumn<Proceso, String> tc_timeMaquina;
 
     @FXML
     private ImageView btn_record;
 
     @FXML
-    private TableColumn<?, ?> c_costoh;
+    private TableColumn<Maquinaria, String> c_costoh;
 
     @FXML
     private ImageView btn_mrpu;
@@ -35,31 +46,31 @@ public class ProcesosController implements Initializable{
     private ImageView btn_recipe;
 
     @FXML
-    private TableColumn<?, ?> c_costot;
+    private TableColumn<Maquinaria, String> c_costot;
 
     @FXML
     private ImageView btn_lista;
 
     @FXML
-    private TableColumn<?, ?> ct_precioH;
+    private TableColumn<Suministro, String> ct_precioH;
 
     @FXML
-    private TableView<?> tb_sumin;
+    private TableView<Suministro> tb_sumin;
 
     @FXML
-    private TableColumn<?, ?> c_timevida;
+    private TableColumn<Maquinaria, String> c_timevida;
 
     @FXML
     private Pane p_demanda;
 
     @FXML
-    private TableColumn<?, ?> tc_detail;
+    private TableColumn<Proceso, String> tc_detail;
 
     @FXML
-    private TableColumn<?, ?> tc_timeM;
+    private TableColumn<Proceso, String> tc_timeM;
 
     @FXML
-    private TableColumn<?, ?> tc_nombre;
+    private TableColumn<Proceso, String> tc_nombre;
 
     @FXML
     private AnchorPane hp_options1;
@@ -71,35 +82,42 @@ public class ProcesosController implements Initializable{
     private ImageView btn_shut;
 
     @FXML
-    private TableColumn<?, ?> tc_maquina;
+    private TableColumn<Proceso, String> tc_maquina;
 
     @FXML
-    private TableView<?> tb_procesos;
+    private TableView<Proceso> tb_procesos;
 
     @FXML
-    private TableColumn<?, ?> tc_num;
+    private TableColumn<Proceso, String> tc_num;
 
     @FXML
     private ImageView btn_output;
 
     @FXML
-    private TableColumn<?, ?> ct_sumi;
+    private TableColumn<Suministro, String> ct_sumi;
 
     @FXML
     private ImageView btn_input;
 
     @FXML
-    private TableColumn<?, ?> c_nombremaq;
+    private TableColumn<Maquinaria, String> c_nombremaq;
 
     @FXML
-    private TableView<?> tb_maquinaria;
+    private TableView<Maquinaria> tb_maquinaria;
 
     @FXML
-    private TableColumn<?, ?> tc_timet;
+    private TableColumn<Proceso, String> tc_timet;
+    private ObservableList<Maquinaria> list4;
+    private ObservableList<Proceso> list5;
+    private ObservableList<Suministro> list6;
     
     
    @Override
     public void initialize(URL location, ResourceBundle resources) {
+        
+        this.llenarMaqu();
+        this.llenarProc();
+        this.llenarSum();
         
     }
 
@@ -165,6 +183,70 @@ public class ProcesosController implements Initializable{
             e.printStackTrace();
         }
          
+    }
+     
+     private void llenarMaqu() {
+        
+        try{
+            
+        ArrayList<Maquinaria> hts =ReturnEntitiesConexion.ReturnMaqu(); 
+        list4 = FXCollections.observableArrayList(hts);
+            
+        }
+        catch(Exception e) {
+        
+            System.out.println(e);
+        
+        }
+        
+        this.c_nombremaq.setCellValueFactory(new PropertyValueFactory<Maquinaria, String>("Maquina"));
+        this.c_timevida.setCellValueFactory(new PropertyValueFactory<Maquinaria, String>("TiempoVida"));
+        this.c_costot.setCellValueFactory(new PropertyValueFactory<Maquinaria, String>("CostoTotal"));
+        this.c_costoh.setCellValueFactory(new PropertyValueFactory<Maquinaria, String>("CostoHora"));
+        this.tb_maquinaria.setItems(list4);
+    }
+     
+     private void llenarProc() {
+        
+        try{
+            
+        ArrayList<Proceso> hts =ReturnEntitiesConexion.ReturnProceso(); 
+        list5 = FXCollections.observableArrayList(hts);
+            
+        }
+        catch(Exception e) {
+        
+            System.out.println(e);
+        
+        }
+        
+        this.tc_num.setCellValueFactory(new PropertyValueFactory<Proceso, String>("idProceso"));
+        this.tc_nombre.setCellValueFactory(new PropertyValueFactory<Proceso, String>("nombre"));
+        this.tc_detail.setCellValueFactory(new PropertyValueFactory<Proceso, String>("detalle"));
+        this.tc_timet.setCellValueFactory(new PropertyValueFactory<Proceso, String>("TiempoTotal"));
+        this.tc_timeM.setCellValueFactory(new PropertyValueFactory<Proceso, String>("TiempoManoObra"));
+        this.tc_maquina.setCellValueFactory(new PropertyValueFactory<Proceso, String>("Maquinaria"));
+        this.tc_timeMaquina.setCellValueFactory(new PropertyValueFactory<Proceso, String>("TiempoMaquinaria"));
+        this.tb_procesos.setItems(list5);
+    }
+     
+     private void llenarSum() {
+        
+        try{
+            
+        ArrayList<Suministro> hts =ReturnEntitiesConexion.ReturnSum(); 
+        list6 = FXCollections.observableArrayList(hts);
+            
+        }
+        catch(Exception e) {
+        
+            System.out.println(e);
+        
+        }
+        
+        this.ct_sumi.setCellValueFactory(new PropertyValueFactory<Suministro, String>("Nombre"));
+        this.ct_precioH.setCellValueFactory(new PropertyValueFactory<Suministro, String>("PrecioHora"));
+        this.tb_sumin.setItems(list6);
     }
 
 
